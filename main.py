19665648c3ad16f8bbbcf745e9e5c1c066a1f05f
Alberto Schmalbach <albert2550@hotmail.com>
2023-01-26 21:25:27 +0000

    Añadiendo al repositorio externo

---
+++ main.py
@@ -0,0 +1,16 @@
+def suma(a:int, b:int)-> int:
+    """Permite sumar 2 numeros enteros
+
+    Args:
+        a (int): n1
+        b (int): n2
+
+    Returns:
+        int: La suma de a + b
+    """
+    result = a + b
+    return result
+
+#print(suma(25, 40))
+#print(suma(5, 4))
+#print(suma(10, 20))
---
+++ operaciones.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+def resta(a: int, b:int)-> int:
+    '''Permite restar 2 numeros'''
+    return a - b
+    
+resultado= resta(100, 40)
+def suma(a:int, b:int)-> int:
+    '''permite sumar 2 numeros'''
+    return a - b
+    
+resultado= suma(50, 30)
+
+print(resultado)
